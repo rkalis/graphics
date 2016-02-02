15013739ba1f8d6d0bee68c97adef91ba9bdbaff
Rosco Kalis <roscokalis@gmail.com>
2016-02-03 00:06:51 +0100

    Changed to integer operations as an optimisation

--- Assignment_1/triangle_rasterization/trirast.c
+++ Assignment_1/triangle_rasterization/trirast.c
@@ -30,22 +30,28 @@
 /* Defines the offscreen point for drawing edges */ 
 int OFFSCREEN[] = {-1, -1};
 
+/* This returns the maximum value of three passed argument by calling the fmax
+ * function twice.
+ */
 float max_3(float a, float b, float c) {
     float max_a_b = fmax(a, b);
     return fmax(max_a_b, c);
 }
 
+/* This returns the minimum value of three passed argument by calling the fmin
+ * function twice.
+ */
 float min_3(float a, float b, float c) {
     float min_a_b = fmin(a, b);
     return fmin(min_a_b, c);
 }
 
-float f(float x0, float x1, float y0, float y1, int x, int y) {
-    return (y0 - y1) * x + (x1 - x0) * y + x0 * y1 - x1 * y0;
+float f(float xa, float xb, float ya, float yb, int x, int y) {
+    return (ya - yb) * x + (xb - xa) * y + xa * yb - xb * ya;
 }
 
-float f_incr(float x0, float x1, float y0, float y1, float f) {
-    return (y0 - y1 + x1 - x0) / f;
+int f_incr(float xa, float xb, float ya, float yb, float sign) {
+    return (int) ((ya - yb + xb - xa) * sign);
 }
 
 void draw_triangle(float x0, float y0, float x1, float y1, float x2, float y2,
@@ -94,10 +100,10 @@ void draw_triangle_optimized(float x0, float y0, float x1, float y1, float x2, f
     int ymin = (int) floor(min_3(y0, y1, y2));
     int ymax = (int) ceil(max_3(y0, y1, y2));
 
-    /* Finds the sign of all points in respect to the opposing line */
-    float alpha_sign = 1;
-    float beta_sign  = 1;
-    float gamma_sign = 1;
+    /* Finds the sign of all points in respect to the lines */
+    int alpha_sign = 1;
+    int beta_sign  = 1;
+    int gamma_sign = 1;
     if(signbit(f(x1, x2, y1, y2, x0, y0))) 
         alpha_sign = -1;
     if(signbit(f(x2, x0, y2, y0, x1, y1)))
@@ -105,34 +111,35 @@ void draw_triangle_optimized(float x0, float y0, float x1, float y1, float x2, f
     if(signbit(f(x0, x1, y0, y1, x2, y2)))
         gamma_sign = -1;
 
-    /* Equations for the offscreen points in respect to the 
-     * lines of the triangle */
-    float alpha_sign_offscreen = f(x1, x2, y1, y2, OFFSCREEN[0], OFFSCREEN[1]);
-    float beta_sign_offscreen  = f(x2, x0, y2, y0, OFFSCREEN[0], OFFSCREEN[1]);
-    float gamma_sign_offscreen = f(x0, x1, y0, y1, OFFSCREEN[0], OFFSCREEN[1]);
+    /* Finds the sign of the offscreen point in respect to the lines */
+    int alpha_sign_offscreen = 1;
+    int beta_sign_offscreen  = 1;
+    int gamma_sign_offscreen = 1;
+
+    if(signbit(f(x1, x2, y1, y2, OFFSCREEN[0], OFFSCREEN[1])))
+        alpha_sign_offscreen = -1;
+    if(signbit(f(x2, x0, y2, y0, OFFSCREEN[0], OFFSCREEN[1])))
+        beta_sign_offscreen = -1;
+    if(signbit(f(x0, x1, y0, y1, OFFSCREEN[0], OFFSCREEN[1])))
+        gamma_sign_offscreen = -1;
 
     for(int y = ymin; y <= ymax; y++) {
-        float alpha = f(x1, x2, y1, y2, xmin - 1, y) * alpha_sign;
-        float beta  = f(x2, x0, y2, y0, xmin - 1, y) * beta_sign;
-        float gamma = f(x0, x1, y0, y1, xmin - 1, y) * gamma_sign;
+        int alpha = f(x1, x2, y1, y2, xmin - 1, y) * alpha_sign;
+        int beta  = f(x2, x0, y2, y0, xmin - 1, y) * beta_sign;
+        int gamma = f(x0, x1, y0, y1, xmin - 1, y) * gamma_sign;
         for(int x = xmin; x <= xmax; x++) {
             alpha += f_incr(0, 0, y1, y2, alpha_sign);
-            // if(alpha < 0) continue;
             beta += f_incr(0, 0, y2, y0, beta_sign);
-            // if(beta < 0) continue;
             gamma += f_incr(0, 0, y0, y1, gamma_sign);
-            // if(gamma < 0) continue;
-
-            // printf("x: %d, y: %d, alpha: %f, beta: %f, gamma: %f\n", x, y, alpha, beta, gamma);
 
             if(alpha >= 0 && beta >= 0 && gamma >= 0) {
-            if((alpha > 0 || alpha_sign * alpha_sign_offscreen > 0)
-            && (beta > 0 || beta_sign * beta_sign_offscreen > 0)
-            && (gamma > 0 || gamma_sign * gamma_sign_offscreen > 0)) {
-            // if(alpha > 0 && beta > 0 && gamma > 0) {
-                PutPixel(x, y, r, g, b);
-                // PutPixel(x, y, 255 * alpha, 255 * beta, 255 * gamma);
-            }
+                if((alpha > 0 || alpha_sign * alpha_sign_offscreen > 0)
+                && (beta  > 0 || beta_sign * beta_sign_offscreen > 0)
+                && (gamma > 0 || gamma_sign * gamma_sign_offscreen > 0)) {
+                // if(alpha > 0 && beta > 0 && gamma > 0) {
+                    PutPixel(x, y, r, g, b);
+                    // PutPixel(x, y, 255 * alpha, 255 * beta, 255 * gamma);
+                }
             }
         }
     }
