79cba4677515e4763fa2706db5930fbde15cfcdc
Rosco Kalis <roscokalis@gmail.com>
2016-02-05 13:45:35 +0100

    Added further comments

--- Assignment_1/basic_midpoint/mla.c
+++ Assignment_1/basic_midpoint/mla.c
@@ -1,46 +1,32 @@
 /* Computer Graphics, Assignment 1, Bresenham's Midpoint Line-Algorithm
-*
-* Filename ........ mla.c
-* Description ..... Midpoint Line Algorithm
-* Created by ...... Jurgen Sturm 
-*
-* Student name ....
-* Student email ... 
-* Collegekaart ....
-* Date ............
-* Comments ........
-*
-*
-* (always fill in these fields before submitting!!)
-*/
+ *
+ * Filename ........ mla.c
+ * STUDENT 1:
+ * Student name .... Robert Jan Schlimbach
+ * Student email ... robertjan.schlimbach@student.uva.nl
+ * Student ID ...... 10802126
+ *
+ * STUDENT 2:
+ * Student name .... Rosco Kalis
+ * Student email ... rosco.kalis@student.uva.nl
+ * Student ID ...... 10771603
+ *
+ */
 
 #include "SDL.h"   
 #include "init.h"
 #include "stdbool.h"
 
-/*
-* Midpoint Line Algorithm
-*
-* As you probably will have figured out, this is the part where you prove
-* your programming skills. The code in the mla function should draw a direct 
-* line between (x0,y0) and (x1,y1) in the specified color. 
-* 
-* Until now, the example code below draws only a horizontal line between
-* (x0,y0) and (x1,y0) and a vertical line between (x1,y1).
-* 
-* And here the challenge begins..
-*
-* Good luck!
-*
-*
-*/
+/* Midpoint Line Algorithm
+ * Draws lines in all directions of a circle
+ */
 void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour) {
-    
-    
 
     PutPixel(s, x1, y1, colour);
 
-    // If the direction of line in the x direction is negative, reverse the points.
+    /* If the direction of line in the x direction is negative,
+     * reverse the points.
+     */
     if (x1 < x0){
         int temp_x = x1;
         int temp_y = y1;
@@ -50,7 +36,9 @@ void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour) {
         y0 = temp_y;
     }
 
-    // If the direction of the line in the y direction is negative, switch y0 and y1.
+    /* If the direction of the line in the y direction is negative, 
+     * switch y0 and y1.
+     */
     bool negative = false;
     if((y1-y0)<0){
         negative = true;
@@ -59,8 +47,9 @@ void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour) {
         y0 = temp;
     }
 
-    // If the slope of the line in either the positive or negative direction is larger than 45 degrees, 
-    // switch the x and y coordinates.
+    /* If the slope of the line in either the positive or negative direction is 
+     * larger than 45 degrees, switch the x and y coordinates.
+     */
     bool steep = false;
     if (abs(y1 - y0) > abs(x1 - x0)){
         steep = true;
@@ -77,9 +66,8 @@ void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour) {
     int x,y;
     y = y0;
     
-    // Calculate the middle line point.
+    /* Calculate the middle line point. */
     int d = (y0 - y1)*(x0 + 1) + (x1 - x0)*(y0 + 0.5) + (x0 * y1) - (x1 * y0);
-
 
     for (x = x0; x <= x1; x++){
         int temp_y;
--- Assignment_1/triangle_rasterization/trirast.c
+++ Assignment_1/triangle_rasterization/trirast.c
@@ -20,13 +20,6 @@
 
 #include "types.h"
 
-/*
- * Rasterize a single triangle.
- * The triangle is specified by its corner coordinates
- * (x0,y0), (x1,y1) and (x2,y2).
- * The triangle is drawn in color (r,g,b).
- */
-
 /* Defines the offscreen point for drawing edges */ 
 int OFFSCREEN[] = {-1, -1};
 
@@ -46,14 +39,24 @@ float min_3(float a, float b, float c) {
     return fmin(min_a_b, c);
 }
 
+/* The function that checks whether a point (x, y) is on a line
+ * defined by the implicit equation:
+ *    (ya - yb) * x + (xb - xa) * y + xa * yb - xb * ya = 0
+ */
 float f(float xa, float xb, float ya, float yb, int x, int y) {
     return (ya - yb) * x + (xb - xa) * y + xa * yb - xb * ya;
 }
 
+/* Returns the increment of one step in the line defined by function f */
 int f_incr(float xa, float xb, float ya, float yb, float sign) {
     return (int) ((ya - yb + xb - xa) * sign);
 }
 
+/* This draws a triangle with points (x0, y0), (x1, y1), (x2, y2) in the colour
+ * defined by (r, g, b). It uses the computation of barycentric coordinates
+ * to check whether each point is inside the triangle. It only checks points
+ * that are inside the bounding box of the triangle.
+ */
 void draw_triangle(float x0, float y0, float x1, float y1, float x2, float y2,
     byte r, byte g, byte b) {
 
@@ -65,7 +68,8 @@ void draw_triangle(float x0, float y0, float x1, float y1, float x2, float y2,
 
     float alpha, beta, gamma;
 
-    /* Equations for the points in respect to the lines of the triangle */
+    /* Equations for the alpha, beta and gamma points in respect to the lines
+     * of the triangle */
     float alpha_function = f(x1, x2, y1, y2, x0, y0);
     float beta_function = f(x2, x0, y2, y0, x1, y1);
     float gamma_function = f(x0, x1, y0, y1, x2, y2);
@@ -78,11 +82,20 @@ void draw_triangle(float x0, float y0, float x1, float y1, float x2, float y2,
 
     for(int y = ymin; y <= ymax; y++) {
         for(int x = xmin; x <= xmax; x++) {
+            /* Computes the new alpha, beta and gamma values and divides by
+             * The function value of the corresponding lines to get a value
+             * between 0 and 1 when it is inside the triangle, below zero
+             * when not.
+             */
             alpha = f(x1, x2, y1, y2, x, y) / alpha_function;
             beta = f(x2, x0, y2, y0, x, y) / beta_function;
             gamma = f(x0, x1, y0, y1, x, y) / gamma_function;
 
             if(alpha >= 0 && beta >= 0 && gamma >= 0) {
+                /* Only draw if it's inside the triangle, or on the triangle edge
+                 * if the triangle is on the side of the arbitrarily chosen
+                 * offscreen point. This prevents shared edges being drawn twice
+                 */
                 if((alpha > 0 || alpha_function * alpha_function_offscreen > 0)
                 && (beta > 0 || beta_function * beta_function_offscreen > 0)
                 && (gamma > 0 || gamma_function * gamma_function_offscreen > 0)) {
@@ -93,6 +106,16 @@ void draw_triangle(float x0, float y0, float x1, float y1, float x2, float y2,
     }
 }
 
+/* An optimised version of the draw_triangle function. It uses an incremental
+ * approach to computing the barycentric coordinates, only computing a start
+ * value and then incrementing that value every step along the way, using
+ * f_incr. It also gets rid of all floating point operations, favouring
+ * integers instead. This means that the alpha, beta and gamma values are not
+ * between 0 and 1 when a point is within the triangle, but instead it is just
+ * greater than or equal to 0, so it can still be tested equally well.
+ * If you were to use it for interpolation (eg. of colours) however, you would
+ * run into problems.
+ */
 void draw_triangle_optimized(float x0, float y0, float x1, float y1, float x2, float y2,
     byte r, byte g, byte b) {
 
@@ -125,6 +148,10 @@ void draw_triangle_optimized(float x0, float y0, float x1, float y1, float x2, f
     if(signbit(f(x0, x1, y0, y1, OFFSCREEN[0], OFFSCREEN[1])))
         gamma_sign_offscreen = -1;
 
+    /* Computes the starting values for alph, beta and gamma, for the point
+     * (xmin - 1, ymin - 1) so that it will be incremented to the correct
+     * starting value in the first loop cycle.
+     */
     int alpha = f(x1, x2, y1, y2, xmin - 1, ymin - 1) * alpha_sign;
     int beta  = f(x2, x0, y2, y0, xmin - 1, ymin - 1) * beta_sign;
     int gamma = f(x0, x1, y0, y1, xmin - 1, ymin - 1) * gamma_sign;
@@ -133,21 +160,29 @@ void draw_triangle_optimized(float x0, float y0, float x1, float y1, float x2, f
         alpha += f_incr(x1, x2, 0, 0, alpha_sign);
         beta += f_incr(x2, x0, 0, 0, beta_sign);
         gamma += f_incr(x0, x1, 0, 0, gamma_sign);
+
+        /* Saves the current values to jump back to after the
+         * x loop is finished
+         */
         int prev_alpha = alpha;
         int prev_beta = beta;
         int prev_gamma = gamma;
+
         for(int x = xmin; x <= xmax; x++) {
             alpha += f_incr(0, 0, y1, y2, alpha_sign);
             beta += f_incr(0, 0, y2, y0, beta_sign);
             gamma += f_incr(0, 0, y0, y1, gamma_sign);
 
+
             if(alpha >= 0 && beta >= 0 && gamma >= 0) {
+                /* Only draw if it's inside the triangle, or on the triangle edge
+                 * if the triangle is on the side of the arbitrarily chosen
+                 * offscreen point. This prevents shared edges being drawn twice
+                 */
                 if((alpha > 0 || alpha_sign * alpha_sign_offscreen > 0)
                 && (beta  > 0 || beta_sign * beta_sign_offscreen > 0)
                 && (gamma > 0 || gamma_sign * gamma_sign_offscreen > 0)) {
-                // if(alpha > 0 && beta > 0 && gamma > 0) {
                     PutPixel(x, y, r, g, b);
-                    // PutPixel(x, y, 255 * alpha, 255 * beta, 255 * gamma);
                 }
             }
         }
