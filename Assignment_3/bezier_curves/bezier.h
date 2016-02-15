616f4b8095040d5051d0db256372447da4c67eac
Rosco Kalis <roscokalis@gmail.com>
2016-02-15 11:45:11 +0100

    Added the framework, edited for use with OS X and Linux, also added in the completed orthogonal projection assignment from last assignment

---
+++ Assignment_3/bezier_curves/bezier.h
@@ -0,0 +1,22 @@
+#ifndef BEZIER_H
+#define BEZIER_H
+
+#ifdef OS_X
+#include <OpenGL/gl.h>
+#else
+#include <GL/gl.h>
+#endif
+
+// A single control point of a Bezier curve
+typedef struct control_point
+{
+    float x;
+    float y;
+}
+control_point;
+
+void    evaluate_bezier_curve(float *x, float *y, control_point p[], int num_points, float u);
+void    draw_bezier_curve(int num_segments, control_point p[], int num_points);
+int     intersect_cubic_bezier_curve(float *y, control_point p[], float x);
+
+#endif
