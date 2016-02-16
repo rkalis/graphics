66de7669af7c15fb60ca5da7ecf10c23cf6dddda
Rosco Kalis <roscokalis@gmail.com>
2016-02-16 15:02:56 +0100

    Added our names to the files, chanegd comments and code style a bit, finished teh first part of the assignment

--- Assignment_3/bezier_curves/bezier.c
+++ Assignment_3/bezier_curves/bezier.c
@@ -4,59 +4,65 @@
  * Date ............ 22.07.2009
  * Created by ...... Paul Melis
  *
- * Student name ....
- * Student email ...
- * Collegekaart ....
- * Date ............
- * Comments ........
+ * STUDENT 1:
+ * Student name .... Robert Jan Schlimbach
+ * Student email ... robertjan.schlimbach@student.uva.nl
+ * Student ID ...... 10802126
  *
+ * STUDENT 2:
+ * Student name .... Rosco Kalis
+ * Student email ... rosco.kalis@student.uva.nl
+ * Student ID ...... 10771603
  *
- * (always fill in these fields before submitting!!)
  */
 
 #include <math.h>
 #include "bezier.h"
 #include <stdio.h>
 
-/* Given a Bezier curve defined by the 'num_points' control points
- * in 'p' compute the position of the point on the curve for parameter
- * value 'u'.
- *
- * Return the x and y values of the point by setting *x and *y,
- * respectively.
- */
-
-void
-evaluate_bezier_curve(float *x, float *y, control_point p[], int num_points, float u)
-{
-    *x = 0.0;
-    *y = 0.0;
+/* Returns the factorial of given number n */
+int factorial(int n) {
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+        fact = fact * i;
+    return fact;
 }
 
-/* Draw a Bezier curve defined by the control points in p[], which
+/* Given a Bezier curve defined by the 'num_points' control points
+ * in 'p' computes the position of the point on the curve for parameter
+ * value 'u'.
+ * Returns the x and y values of the point by setting *x and *y, respectively.
+ */
+void evaluate_bezier_curve(float *x, float *y, control_point p[], int num_points, float u) {
+    *x = 0.0;
+    *y = 0.0;
+    int n = num_points - 1;
+    for(int k = 0; k <= n; k++) {
+        float coef = (factorial(n)) / (factorial(k) * factorial(n - k));
+        float sum = pow(u, k) * pow((1 - u), (n - k));
+        *x += p[k].x * coef * sum;
+        *y += p[k].y * coef * sum;
+    }
+}
+
+/* Draws a Bezier curve defined by the control points in p[], which
  * will contain 'num_points' points.
- *
- * Draw the line segments you compute directly on the screen
- * as a single GL_LINE_STRIP. This is as simple as using
- *
- *      glBegin(GL_LINE_STRIP);
- *      glVertex2f(..., ...);
- *      ...
- *      glEnd();
- *
- * DO NOT SET ANY COLOR!
- *
+ * Draws the line segments you compute directly on the screen
+ * as a single GL_LINE_STRIP.
  * The 'num_segments' parameter determines the "discretization" of the Bezier
  * curve and is the number of straight line segments that should be used
  * to approximate the curve.
- *
- * Call evaluate_bezier_curve() to compute the necessary points on
+ * Calls evaluate_bezier_curve() to compute the necessary points on
  * the curve.
  */
-
-void
-draw_bezier_curve(int num_segments, control_point p[], int num_points)
-{
+void draw_bezier_curve(int num_segments, control_point p[], int num_points) {
+    float x, y;
+    glBegin(GL_LINE_STRIP);
+    for(int i = 0; i <= num_segments; i++) {
+        evaluate_bezier_curve(&x, &y, p, num_points, (float) i / (float) num_segments);
+        glVertex2f(x, y);
+    }
+    glEnd();
 }
 
 /* Find the intersection of a cubic Bezier curve with the line X=x.
@@ -65,9 +71,7 @@ draw_bezier_curve(int num_segments, control_point p[], int num_points)
    Return 0 if no intersection exists.
 */
 
-int
-intersect_cubic_bezier_curve(float *y, control_point p[], float x)
-{
+int intersect_cubic_bezier_curve(float *y, control_point p[], float x) {
     return 0;
 }
 
--- Assignment_3/bezier_curves/singlecurve.c
+++ Assignment_3/bezier_curves/singlecurve.c
@@ -4,14 +4,16 @@
  * Date ............ 22.07.2009
  * Created by ...... Paul Melis
  *
- * Student name ....
- * Student email ...
- * Collegekaart ....
- * Date ............
- * Comments ........
+ * STUDENT 1:
+ * Student name .... Robert Jan Schlimbach
+ * Student email ... robertjan.schlimbach@student.uva.nl
+ * Student ID ...... 10802126
  *
+ * STUDENT 2:
+ * Student name .... Rosco Kalis
+ * Student email ... rosco.kalis@student.uva.nl
+ * Student ID ...... 10771603
  *
- * (always fill in these fields before submitting!!)
  */
 
 #ifdef OS_X
