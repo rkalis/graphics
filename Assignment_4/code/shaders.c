bb91032c44b6fea2f67b122ebd6e7c218597caa6
Rosco Kalis <roscokalis@gmail.com>
2016-03-02 11:41:57 +0100

    Added shadows to the matte shader

--- Assignment_4/code/shaders.c
+++ Assignment_4/code/shaders.c
@@ -35,15 +35,27 @@ shade_constant(intersection_point ip)
     return v3_create(1, 0, 0);
 }
 
-vec3
-shade_matte(intersection_point ip)
-{
+/* Uses a simple matte surface shader with shadows */
+vec3 shade_matte(intersection_point ip) {
+    /* Start with ambient light */
     float contribution = scene_ambient_light;
+
     for(int i = 0; i < scene_num_lights; i++) {
+        /* Create the vector between the ip and the light and normalize it */
         vec3 light_vector = v3_subtract(scene_lights[i].position, ip.p);
         light_vector = v3_normalize(light_vector);
-        float light_contribution = v3_dotprod(ip.n, light_vector) * scene_lights[i].intensity;
-        if(light_contribution > 0) {
+
+        /* Check for shadows from two different origins to prevent self-shading
+         * on the balls */
+        vec3 ray_origin1 = v3_add(ip.p, v3_create(0.001, 0.001, 0.001));
+        vec3 ray_origin2 = v3_add(ip.p, v3_create(-0.001, -0.001, -0.001));
+        int shadow = shadow_check(ray_origin1, light_vector);
+        shadow += shadow_check(ray_origin2, light_vector);
+
+        /* If there is light contribution and the ip isn't in the shadow
+         * add the contribution of the light to the overall contribution */
+        if(light_contribution > 0 &&  shadow < 2) {
+            float light_contribution = v3_dotprod(ip.n, light_vector) * scene_lights[i].intensity;
             contribution += light_contribution;
         }
     }
