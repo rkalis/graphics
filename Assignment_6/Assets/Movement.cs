4cb9c9b197bd1d236ad418e7800494b375c7a0e1
Rosco Kalis <roscokalis@gmail.com>
2016-03-12 23:48:47 +0100

    Added horizontal movement

---
+++ Assignment_6/Assets/Movement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	public float speed = 5.0f;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float hor = Input.GetAxisRaw ("Horizontal") * speed * Time.deltaTime;
+		Vector3 translation = new Vector3 (hor, 0);
+		transform.Translate (translation);
+	}
+}
