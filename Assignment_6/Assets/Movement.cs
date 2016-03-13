96342c92cf1875bd10b5ffc517a8f0ea9b19dce3
Rosco Kalis <roscokalis@gmail.com>
2016-03-13 18:53:52 +0100

    Added basic jumping, you can jump from anywhere (including the air), also changed the way movement is implemented

--- Assignment_6/Assets/Movement.cs
+++ Assignment_6/Assets/Movement.cs
@@ -2,16 +2,24 @@
 using System.Collections;
 
 public class Movement : MonoBehaviour {
-	public float speed = 5.0f;
+	public float speed = 10.0f;
+	public float jumpSpeed = 5.0f;
+
 	// Use this for initialization
 	void Start () {
-
 	}
 
 	// Update is called once per frame
-	void Update () {
-		float hor = Input.GetAxisRaw ("Horizontal") * speed * Time.deltaTime;
-		Vector3 translation = new Vector3 (hor, 0);
-		transform.Translate (translation);
+	void FixedUpdate () {
+		Rigidbody2D body = GetComponent<Rigidbody2D> ();
+		Vector2 velocity = body.velocity;
+		float xVelocity = Input.GetAxisRaw ("Horizontal") * speed;
+		velocity.x = xVelocity;
+
+		if (Input.GetAxisRaw ("Vertical") == 1) {
+			velocity.y = jumpSpeed;
+		}
+		body.velocity = velocity;
+
 	}
 }
