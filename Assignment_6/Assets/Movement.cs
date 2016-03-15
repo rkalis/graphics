5e79d00e7bb8be928e6af8649b7c0065e0020372
Rosco Kalis <roscokalis@gmail.com>
2016-03-15 15:02:27 +0100

    Added the need to be grounded to jump, gitignored Library

--- Assignment_6/Assets/Movement.cs
+++ Assignment_6/Assets/Movement.cs
@@ -4,9 +4,26 @@ using System.Collections;
 public class Movement : MonoBehaviour {
 	public float speed = 10.0f;
 	public float jumpSpeed = 5.0f;
+	private bool colliding;
+	private bool grounded;
 
 	// Use this for initialization
 	void Start () {
+		colliding = false;
+		grounded = false;
+	}
+		
+	void OnCollisionStay2D(Collision2D coll) {
+		Rigidbody2D body = GetComponent<Rigidbody2D> ();
+		if (coll.contacts [0].point.y < body.position.y &&
+			coll.contacts [1].point.y < body.position.y) {
+			grounded = true;
+		}
+		colliding = true;
+	}
+	void OnCollisionExit2D(Collision2D coll) {
+		colliding = false;
+		grounded = false;
 	}
 
 	// Update is called once per frame
@@ -16,7 +33,7 @@ public class Movement : MonoBehaviour {
 		float xVelocity = Input.GetAxisRaw ("Horizontal") * speed;
 		velocity.x = xVelocity;
 
-		if (Input.GetAxisRaw ("Vertical") == 1) {
+		if (Input.GetAxisRaw ("Vertical") == 1 && grounded) {
 			velocity.y = jumpSpeed;
 		}
 		body.velocity = velocity;
