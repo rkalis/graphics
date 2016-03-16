25f291206c6403a8a9088597e5f0c525847e2762
Rosco Kalis <roscokalis@gmail.com>
2016-03-16 12:44:40 +0100

    Added full Unity gitignore, as well as adding one way platforms

---
+++ Assignment_6/Assets/OneWayPlatformTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OneWayPlatformTrigger : MonoBehaviour {
+	public GameObject platform;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		platform.GetComponent<BoxCollider2D> ().isTrigger = true;
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		platform.GetComponent<BoxCollider2D> ().isTrigger = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
